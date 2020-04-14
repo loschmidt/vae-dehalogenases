4fde8604aaf1838016cdce7c4ef2e0326de75025
Pavel Kohout <xkohou15@stud.fit.vutbr.cz>
2020-04-14 14:05:57 +0200

    [Feature]: Download PF all seed full RP15 -> RP75
    Signed-off-by: Pavel Kohout <xkohou15@stud.fit.vutbr.cz>

--- vae_our_approach/pfam_msa/script/download_MSA.py
+++ vae_our_approach/pfam_msa/script/download_MSA.py
@@ -11,25 +11,30 @@ import sys
 import argparse
 from Bio import SeqIO
 
+FASTA_GENERATE = False
+
 parser = argparse.ArgumentParser(description = "Download the full multiple sequence alignment (MSA) in Stockholm format for a Pfam_id.")
 parser.add_argument("--Pfam_id", help = "the ID of Pfam; e.g. PF00041")
 args = parser.parse_args()
 pfam_id = args.Pfam_id
 
+down_seq = ["full", "RP75", "RP55", "RP35", "RP15", "seed"]
 
-print("Downloading the full multiple sequence alignment for Pfam: {0} ......".format(pfam_id))
-http = urllib3.PoolManager()
-r = http.request('GET', 'http://pfam.xfam.org/family/{0}/alignment/full/gzipped'.format(pfam_id))
-data = gzip.decompress(r.data)
-data = data.decode()
-with open("./MSA/{0}_full.txt".format(pfam_id), 'w') as file_handle:
-    print(data, file = file_handle)
+for dow in down_seq:
+    print("Downloading the {1} multiple sequence alignment for Pfam: {0} ......".format(pfam_id, dow))
+    http = urllib3.PoolManager()
+    r = http.request('GET', 'http://pfam.xfam.org/family/{0}/alignment/{1}/gzipped'.format(pfam_id, dow))
+    data = gzip.decompress(r.data)
+    data = data.decode()
+    with open("./MSA/{0}_{1}.txt".format(pfam_id, dow), 'w') as file_handle:
+        print(data, file = file_handle)
 
-stockholm_file_name = "./MSA/{0}_full.txt".format(pfam_id)
-fasta_file_name = "./MSA/{0}_full.fasta".format(pfam_id)
+if FASTA_GENERATE:
+    stockholm_file_name = "./MSA/{0}_full.txt".format(pfam_id)
+    fasta_file_name = "./MSA/{0}_full.fasta".format(pfam_id)
 
-records = SeqIO.parse(stockholm_file_name, "stockholm")
-count = SeqIO.write(records, fasta_file_name, "fasta")
-print("Converted %i records" % count)
+    records = SeqIO.parse(stockholm_file_name, "stockholm")
+    count = SeqIO.write(records, fasta_file_name, "fasta")
+    print("Converted %i records" % count)
 
-print("Convertion done")
+    print("Convertion done")
