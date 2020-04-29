253b8bc7421a885890c3e00e033f1eb047e2b7fb
Pavel Kohout <xkohou15@stud.fit.vutbr.cz>
2020-04-29 21:05:38 +0200

    [Feature]: Adding general script for running noninteractive jobs
    Signed-off-by: Pavel Kohout <xkohou15@stud.fit.vutbr.cz>

--- vae_our_approach/run_train.sh
+++ vae_our_approach/run_train.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
+## This git repository is allocate in DATADIR_BASE/AI-dir/vae-for-hlds
+## Please change for your location
+DATADIR_BASE=/storage/brno2/home/xkohou15
+SCRIPT_RUN=./AI_dir/var-for-hlds/vae_our_approach/pfam_msa
+
 # check number of parameters
 if [ $# == 0 ]; then
 	echo "Please, give pfam sequence ID as parameter of this script"
 	exit 1
 else
-	# Run qsub with desired param
-	echo "Running qsub on GPU node with sequence ${1}"
-	qsub -v seqID=${1} train_vae.sh
+  cd $DATADIR_BASE
+  # Activate venv for download script
+  cd ../../.
+  source .bashrc
+  conda activate #vae-env
+
+  # Download desire sequence and its rps and seed seqs
+  cd $SCRIPT_RUN
+  python3 ./script/download_MSA.py --Pfam_id ${1}
+  cd ../../.
+  # Deactivate venv
+  conda deactivate
+
+  # Run in parallel for multiple nodes, each sub sequence
+  for rp_i in "full" "rp75" "rp55" "rp35" "rp15" "seed"; do
+    # Run qsub with desired param
+    echo "Running qsub on GPU node with sequence ${1} and ${rp_i}"
+    qsub -v seqID=${1} rp=${rp_i} train_vae.sh &
+  done
 fi
 
 
--- vae_our_approach/train_vae.sh
+++ vae_our_approach/train_vae.sh
@@ -6,11 +6,12 @@
 
 # define a DATADIR variable: directory where the input files are taken from and where output will be copied to
 DATADIR_BASE=/storage/brno2/home/xkohou15 # substitute to your home directory
-DATADIR=/storage/brno2/xkohou15/AI_dir/vae_our_aproach/pfam_msa # substitute username and path to to your real username and path
-LOGDIR=/storage/brno2/home/xkohou15/AI_dir/vae_our_aproach # substitute for your log directory
+DATADIR=/storage/brno2/xkohou15/AI_dir/vae-for-hlds/vae_our_aproach/pfam_msa # substitute username and path to to your real username and path
+LOGDIR=/storage/brno2/home/xkohou15/AI_dir/vae-for-hlds/vae_our_aproach/pfam_msa # substitute for your log directory
 
 # Parse passed sequence code throught param
 PFAMSEQ=${seqID}
+RPgroup=${rp}
 
 # append a line to a file "jobs_info.txt" containing the ID of the job, the hostname of node it is run on and the path to a scratch directory
 # this information helps to find a scratch directory in case the job fails and you need to remove the scratch directory manually 
@@ -23,16 +24,16 @@ conda activate #vae-env
 echo "Sourcing anaconda succesful" >> $LOGDIR/jobs_info.txt
 
 cd $DATADIR
-python3 ./script/download_MSA.py --Pfam_id $PFAMSEQ
-echo "Dowload of PFAM succesfull" >> $LOGDIR/jobs_info.txt
+#python3 ./script/download_MSA.py --Pfam_id $PFAMSEQ
+#echo "Dowload of PFAM succesfull" >> $LOGDIR/jobs_info.txt
 
-python3 ./script/proc_msa.py
+python3 ./script/proc_msa.py --Pfam_id $PFAMSEQ --RPgroup ${RPgroup}
 echo "proc_msa succesful" >> $LOGDIR/jobs_info.txt
 
-python3 ./script/train.py --num_epoch 10000 --weight_decay 0.01
+python3 ./script/train.py --num_epoch 10000 --weight_decay 0.01 --Pfam_id $PFAMSEQ --RPgroup ${RPgroup}
 echo "Train completed " >> $LOGDIR/jobs_info.txt
 
-#python3 ./script/analyze_model.py
-#echo "Analyze clean" >> $LOGDIR/jobs_info.txt
+python3 ./script/analyze_model1.py --Pfam_id $PFAMSEQ --RPgroup ${RPgroup}
+echo "Analyze clean" >> $LOGDIR/jobs_info.txt
 # clean the SCRATCH directory
 clean_scratch
