597c46b9aa53d5e5e042d93433ad9b74a543bea7
Pavel Kohout <xkohou15@stud.fit.vutbr.cz>
2020-07-28 10:30:01 +0200

    [FEATURE]: Adding downloader file. Now the cross reference of modules works fine. Next step will be preprocessing of MSA.

---
+++ vae_our_approach/devel_LL/download_MSA.py
@@ -0,0 +1,33 @@
+__author__ = "Pavel Kohout <xkohou15@stud.fit.vutbr.cz>"
+__date__ = "2020/07/28 09:19:00"
+
+""""
+Download the multiple sequence alignment for a given Pfam ID
+"""
+
+import urllib3
+import gzip
+
+from pipeline import StructChecker
+
+class Downloader:
+    def __init__(self, setuper):
+        self.msa_dir = setuper.MSA_fld
+        self.pfam_id = setuper.pfam_id
+        self.down_seq = ["full", "rp75", "rp55", "rp35", "rp15", "seed"]
+        self._download()
+
+    def _download(self):
+        for dow in self.down_seq:
+            print("Downloading the {1} multiple sequence alignment for Pfam: {0} ......".format(self.pfam_id, dow))
+            http = urllib3.PoolManager()
+            r = http.request('GET', 'http://pfam.xfam.org/family/{0}/alignment/{1}/gzipped'.format(self.pfam_id, dow))
+            data = gzip.decompress(r.data)
+            data = data.decode()
+            with open("{0}{1}_{2}.txt".format(self.msa_dir,self.pfam_id, dow), 'w') as file_handle:
+                print(data, file=file_handle)
+
+if __name__ == '__main__':
+    tar_dir = StructChecker()
+    tar_dir.setup_struct()
+    downloader = Downloader(tar_dir)
--- vae_our_approach/devel_LL/pipeline.py
+++ vae_our_approach/devel_LL/pipeline.py
@@ -6,17 +6,18 @@ import subprocess as sp   ## command line handling
 
 class StructChecker:
     def __init__(self):
-        pfam_ID, args = self.get_parser()
+        self.pfam_id, args = self.get_parser()
 
-        self.res_root_fld = "./PIPE_RES/"
-        self.run_name = self.res_root_fld + pfam_ID + "/"
+        self.res_root_fld = "./results/"
+        self.run_name = self.res_root_fld + self.pfam_id + "/"
         self.MSA_fld = self.run_name + "MSA/"
-        self.flds_arr = [self.res_root_fld, self.run_name, self.MSA_fld]
+        self.VAE_model_dir = self.run_name + "model"
+        self.flds_arr = [self.res_root_fld, self.run_name, self.MSA_fld, self.VAE_model_dir]
 
-    def add_to_check(self, folderName):
+    def add_to_dir(self, folderName):
         self.flds_arr.append(folderName)
 
-    def do_not_check(self, folderName):
+    def remove_from_dir(self, folderName):
         self.flds_arr.remove(folderName)
 
     def get_parser(self):
@@ -24,13 +25,20 @@ class StructChecker:
         parser.add_argument("--Pfam_id", help="the ID of Pfam; e.g. PF00041, will create subdir for script data")
         args = parser.parse_args()
         if args.Pfam_id is None:
-            print("Error: Pfam_id parameter is missing!! Please run {0} --Pfam_id \"tip your Pfam ID\"".format(__file__))
+            print("Error: Pfam_id parameter is missing!! Please run {0} --Pfam_id [Pfam ID]".format(__file__))
             exit(1)
         return args.Pfam_id, args
 
-    def check_struct(self):
+    def setup_struct(self):
         for folder in self.flds_arr:
             sp.run("mkdir -p {0}".format(folder), shell=True)
+        print("Directory structure of {0} was successfully prepared".format(self.run_name))
 
 if __name__ == '__main__':
+    ## Our modules imports
+    from download_MSA import Downloader
+
     '''Pipeline of our VAE data preparation, training, executing'''
+    tar_dir = StructChecker()
+    tar_dir.setup_struct()
+    down_MSA = Downloader(tar_dir)
