fe9c0d7cd3430436c0fd6a8436839d9c6d5814da
Pavel Kohout <xkohou15@stud.fit.vutbr.cz>
2020-07-29 13:19:35 +0200

    [FEATURE]: Modifying pipeline parser a its parameter to cover needs of whole pipeline. Download file only if it havent been done already.

--- vae_our_approach/devel_LL/download_MSA.py
+++ vae_our_approach/devel_LL/download_MSA.py
@@ -7,6 +7,7 @@ Download the multiple sequence alignment for a given Pfam ID
 
 import urllib3
 import gzip
+import os.path as path
 
 from pipeline import StructChecker
 
@@ -18,16 +19,22 @@ class Downloader:
         if all:
             self.down_seq = ["full", "rp75", "rp55", "rp35", "rp15", "seed"]
         self._download()
+        setuper.set_msa_file("{0}{1}_{2}.txt".format(self.msa_dir,self.pfam_id, setuper.rp))
 
     def _download(self):
         for dow in self.down_seq:
-            print("Downloading the {1} multiple sequence alignment for Pfam: {0} ......".format(self.pfam_id, dow))
-            http = urllib3.PoolManager()
-            r = http.request('GET', 'http://pfam.xfam.org/family/{0}/alignment/{1}/gzipped'.format(self.pfam_id, dow))
-            data = gzip.decompress(r.data)
-            data = data.decode()
-            with open("{0}{1}_{2}.txt".format(self.msa_dir,self.pfam_id, dow), 'w') as file_handle:
-                print(data, file=file_handle)
+            file_name = "{0}{1}_{2}.txt".format(self.msa_dir,self.pfam_id, dow)
+            if path.isfile(file_name):
+                print("File is already downloaded in {0}".format(file_name))
+                print("If you want to download it again, please delete it from its location")
+            else:
+                print("Downloading the {1} multiple sequence alignment for Pfam: {0} ......".format(self.pfam_id, dow))
+                http = urllib3.PoolManager()
+                r = http.request('GET', 'http://pfam.xfam.org/family/{0}/alignment/{1}/gzipped'.format(self.pfam_id, dow))
+                data = gzip.decompress(r.data)
+                data = data.decode()
+                with open(file_name, 'w') as file_handle:
+                    print(data, file=file_handle)
 
 if __name__ == '__main__':
     tar_dir = StructChecker()
--- vae_our_approach/devel_LL/pipeline.py
+++ vae_our_approach/devel_LL/pipeline.py
@@ -13,6 +13,8 @@ class StructChecker:
         else:
             self.ref_seq = False
             self.ref_n = ""
+        self.keep_gaps = bool(self.args.keep_gaps)
+        self.stats = bool(self.args.stats)
 
         self.res_root_fld = "./results/"
         self.run_root_dir = self.res_root_fld + self.pfam_id + "/"
@@ -26,11 +28,16 @@ class StructChecker:
     def remove_from_dir(self, folderName):
         self.flds_arr.remove(folderName)
 
+    def set_msa_file(self, file_name):
+        self.msa_file = file_name
+
     def get_parser(self):
         parser = argparse.ArgumentParser(description='Parameters for training the model')
         parser.add_argument("--Pfam_id", help="the ID of Pfam; e.g. PF00041, will create subdir for script data")
         parser.add_argument("--RP", help="RP specifier of given Pfam_id family, e.g. RP15, default value is full")
         parser.add_argument("--ref", help="the reference sequence; e.g. TENA_HUMAN/804-884")
+        parser.add_argument("--keep_gaps", help="Setup in case you want to keep all gaps in sequences. Default False", default=False)
+        parser.add_argument("--stats", help="Printing statistics of msa processing. Default False", default=False)
         args = parser.parse_args()
         if args.Pfam_id is None:
             print("Error: Pfam_id parameter is missing!! Please run {0} --Pfam_id [Pfam ID]".format(__file__))
@@ -44,21 +51,27 @@ class StructChecker:
         print("Directory structure of {0} was successfully prepared".format(self.run_root_dir))
 
     def _setup_RP_run(self):
+        """Setups subfolder of current rp group. Sets model, pickles"""
         self.rp = "full"
         if self.args.RP is not None:
             self.rp = self.args.RP
         self.rp_dir = self.run_root_dir + "/" + self.rp
         self.VAE_model_dir = self.rp_dir + "/" + "model"
+        self.pickles_fld = self.rp_dir + "/" + "pickles"
         self.add_to_dir(self.rp_dir)
         self.add_to_dir(self.VAE_model_dir)
+        self.add_to_dir(self.pickles_fld)
         print("The run will be continue with RP \"{0}\" and data will be generated to {1}"
               " (for different rp rerun script with --RP [e.g. rp75] paramater)".format(self.rp, self.rp_dir))
 
 if __name__ == '__main__':
     ## Our modules imports
     from download_MSA import Downloader
+    from msa_prepar import MSA
 
     '''Pipeline of our VAE data preparation, training, executing'''
     tar_dir = StructChecker()
     tar_dir.setup_struct()
     down_MSA = Downloader(tar_dir)
+    msa = MSA(tar_dir)
+    msa.proc_msa()
