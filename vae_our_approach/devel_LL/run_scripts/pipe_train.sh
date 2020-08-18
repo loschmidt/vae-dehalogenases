c5b1db03a0096378ef8d94999615e2cee1eb3074
Pavel Kohout <xkohou15@stud.fit.vutbr.cz>
2020-08-18 09:20:18 +0200

    [Maintenance]: No catalytic options for run
    Signed-off-by: Pavel Kohout <xkohou15@stud.fit.vutbr.cz>

--- vae_our_approach/devel_LL/run_scripts/pipe_train.sh
+++ vae_our_approach/devel_LL/run_scripts/pipe_train.sh
@@ -24,7 +24,7 @@ conda activate #vae-env
 echo "Sourcing anaconda succesful" >> $LOGDIR/jobs_info.txt
 ## Run pipeline
 cd $DATADIR
-python3 pipeline.py --Pfam_id $PFAMSEQ --ref ${QUERY} --output_dir ${QUERY} --preserve_catalytic False >> $LOGDIR/jobs_info.txt
+python3 pipeline.py --Pfam_id $PFAMSEQ --ref ${QUERY} --output_dir ${QUERY} --stats True --no_preserve_catalytic >> $LOGDIR/jobs_info.txt
 
 ##python3 ./script/analyze_model1.py --Pfam_id $PFAMSEQ --RPgroup ${RPgroup}
 echo "Training process is finished" >> $LOGDIR/jobs_info.txt
