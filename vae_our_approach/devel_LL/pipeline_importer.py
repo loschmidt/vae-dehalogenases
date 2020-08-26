b4c420020675750215fbff6386e0c7c4c804e03f
Pavel Kohout <xkohou15@stud.fit.vutbr.cz>
2020-08-26 10:00:39 +0200

    [Feature]: Adding new switcher for ppeline selection.
    Signed-off-by: Pavel Kohout <xkohou15@stud.fit.vutbr.cz>

--- vae_our_approach/devel_LL/pipeline_importer.py
+++ vae_our_approach/devel_LL/pipeline_importer.py
@@ -1,13 +1,18 @@
 import os
 
-if 'PIP_BRANCH' in os.environ:
-    if os.environ['PIP_BRANCH'] == 'True':
+if ['PIP_CAT', 'PIP_PAPER', 'PIP_SCORE'] in os.environ:
+    if os.environ['PIP_CAT'] == 'True':
         from catalytic_res_msa_filter import CatalyticMSAPreprocessor as MSA
-    else:
+    elif os.environ['PIP_PAPER'] == 'True' or os.environ['PIP_SCORE'] == 'False':
+        # Default variant in the case of setting just --no_score_filter param
         from msa_prepar import MSA as MSA
+    elif os.environ['PIP_SCORE'] == 'True':
+        from msa_filter_scorer import MSAFilterCutOff as MSA
 
     ## Unset linux environment variable
-    del os.environ['PIP_BRANCH']
+    del os.environ['PIP_SCORE']
+    del os.environ['PIP_CAT']
+    del os.environ['PIP_PAPER']
 else:
-    print("The os environment variable was not set!!")
+    print("The os environment variables were not set!!")
     exit(1)
