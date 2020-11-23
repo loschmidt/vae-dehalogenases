0f033af97231cef4e2fad0bbba96a6494b666e8f
Pavel Kohout <xkohou15@stud.fit.vutbr.cz>
2020-11-24 00:18:02 +0100

    [Feature]: Adding scripts for remote run of generative model ability measurment

---
+++ vae_our_approach/devel_LL/run_scripts/params_pipe.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#PBS -N Generative
+#PBS -q gpu -l select=1:ngpus=1:mem=8gb:scratch_local=500mb
+#PBS -l walltime=24:00:00
+#PBS -m ae
+
+# define a DATADIR variable: directory where the input files are taken from and where output will be copied to
+DATADIR_BASE=/storage/brno2/home/xkohou15 # substitute to your home directory
+DATADIR=/storage/brno2/xkohou15/AI_dir/vae-for-hlds/vae_our_approach/devel_LL # substitute username and path to to your real username and path
+LOGDIR=/storage/brno2/home/xkohou15/AI_dir/vae-for-hlds/vae_our_approach/devel_LL/devel_log/ # substitute for your log directory
+
+# Parse passed sequence code throught param
+C=${c}
+
+# append a line to a file "jobs_info.txt" containing the ID of the job, the hostname of node it is run on and the path to a scratch directory
+# this information helps to find a scratch directory in case the job fails and you need to remove the scratch directory manually
+echo "$PBS_JOBID is running on node `hostname -f` in a scratch directory $SCRATCHDIR"
+
+# move into data directory
+cd $DATADIR_BASE
+source $DATADIR_BASE/.bashrc
+conda activate #vae-env
+echo "Sourcing anaconda succesful" >> $LOGDIR/jobs_info.txt
+## Run pipeline
+cd $DATADIR
+## python3 pipeline.py --Pfam_id $PFAMSEQ --ref ${QUERY} --output_dir ${QUERY} --stats --highlight_seqs ${QUERY}
+
+echo "========================================================================"
+echo "Training model for given param c = ${C}"
+python3 train.py --Pfam_id PF00561 --output_dir generativeAbility --stats --in_file results/PF00561/MSA/identified_targets_msa.fa --C $C
+
+echo "========================================================================"
+echo "Measurement of generative process has begun"
+python3 benchmark.py --Pfam_id PF00561 --output_dir generativeAbility --C $C
+
+##python3 ./script/analyze_model1.py --Pfam_id $PFAMSEQ --RPgroup ${RPgroup}
+echo "Training process is finished" >> $LOGDIR/jobs_info.txt
+# clean the SCRATCH directory
+clean_scratch
---
+++ vae_our_approach/devel_LL/run_scripts/train_params.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+## This git repository is allocate in DATADIR_BASE/AI-dir/vae-for-hlds
+## Please change for your location
+DATADIR_BASE=/storage/brno2/home/xkohou15
+ROOT_SCR=AI_dir/vae-for-hlds/vae_our_approach/devel_LL/run_scripts
+LOGDIR=/storage/brno2/home/xkohou15/AI_dir/vae-for-hlds/vae_our_approach/devel_LL/devel_log/
+PYTHONSCRIPTS=/storage/brno2/home/xkohou15/AI_dir/vae-for-hlds/vae_our_approach/devel_LL
+
+# check number of parameters
+if [ $# == 0 ]; then
+	echo "Please, give values of C you wish to run with. "
+	exit 1
+else
+  mkdir -p $LOGDIR
+
+  # move into data directory
+  cd $DATADIR_BASE
+  source $DATADIR_BASE/.bashrc
+  conda activate #vae-env
+  echo "Sourcing anaconda succesful" >> $LOGDIR/jobs_info.txt
+
+  cd $PYTHONSCRIPTS
+  python3 msa_filter_scorer.py --Pfam_id PF00561 --ref P59336_S14 --output_dir generativeAbility --stats --in_file results/PF00561/MSA/identified_targets_msa.fa
+
+  cd $ROOT_SCR
+  # Run in parallel for multiple nodes, for each query sequence
+  for c; do
+    # Run qsub with desired param
+    echo "Running qsub on GPU node with C value ${c}"
+    qsub -v c=${c} params_pipe.sh &
+  done
+fi
+
+
