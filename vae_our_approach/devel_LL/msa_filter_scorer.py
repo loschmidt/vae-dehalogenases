bf31b9cc34c9009604251207555f59aa91dc924d
Pavel Kohout <xkohou15@stud.fit.vutbr.cz>
2021-03-04 14:37:47 +0100

    [Maintenance]: Nice print
    Signed-off-by: Pavel Kohout <xkohou15@stud.fit.vutbr.cz>

--- vae_our_approach/devel_LL/msa_filter_scorer.py
+++ vae_our_approach/devel_LL/msa_filter_scorer.py
@@ -286,7 +286,7 @@ class MSAFilterCutOff :
             for i, input in enumerate(inputs):
                 ret_clusters = self.phylo_clustering(input, threshold=th)
                 clusters.extend(ret_clusters)
-                if (i+1) % 50 == 0:
+                if (i+1) % max(50, len(inputs)//10) == 0:
                     print("\tClusters proceeded {}/{}".format(i+1, len(inputs)), flush=True)
             inputs = clusters
             print('MSA_filter message : Clustering with identity {} done, number of clusters {}'.format(th, len(clusters)))
