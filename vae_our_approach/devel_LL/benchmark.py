72b0a1e93a54147ac1316c476e079c94a20ea3d6
Pavel Kohout <xkohou15@stud.fit.vutbr.cz>
2021-03-16 11:35:47 +0100

    [Maintenance]: removing useless checkers for probs bigger than 1
    Signed-off-by: Pavel Kohout <xkohou15@stud.fit.vutbr.cz>

--- vae_our_approach/devel_LL/benchmark.py
+++ vae_our_approach/devel_LL/benchmark.py
@@ -47,17 +47,8 @@ class Benchmarker:
 
     def make_bench(self):
         marginals_train = self._bench(self.train_data)
-        for m in marginals_train:
-            if m > 1:
-                print("Benchmark Message : Train {} > 1")
         marginals_positive = self._bench(self.positive)
-        for m in marginals_positive:
-            if m > 1:
-                print("Benchmark Message : Pos {} > 1")
         marginals_negative = self._bench(self.negative)
-        for m in marginals_negative:
-            if m > 1:
-                print("Benchmark Message : Neg {} > 1")
         self._store_marginals(marginals_train, marginals_positive, marginals_negative)
 
         mean_p = sum(marginals_positive) / len(marginals_positive)
