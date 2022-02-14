8ac6010668193bdcd000a350a82b35c1b2140334
Pavel Kohout <xkohou15@stud.fit.vutbr.cz>
2022-02-14 13:27:43 +0100

    [Feature]: Frequencies calculation
    Signed-off-by: Pavel Kohout <xkohou15@stud.fit.vutbr.cz>

--- vae_our_approach/devel_LL/Statistics/order_statistics.py
+++ vae_our_approach/devel_LL/Statistics/order_statistics.py
@@ -9,6 +9,7 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
+from typing import Tuple
 
 currentDir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentDir = os.path.dirname(currentDir)
@@ -69,9 +70,9 @@ class OrderStatistics:
         """ Creates directory in Highlight results directory """
         os.makedirs(self.target_dir, exist_ok=True)
 
-    def shannon_entropy(self, msa: np.ndarray) -> np.ndarray:
+    def shannon_entropy(self, msa: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         """
-        1st order statistic
+        1st order statistic/frequencies
         Computes Shannon entropy of MSA given as:
             H_j = -SUM(p^a_j * log(p^a_j))
                 p^a_j = n^a_j / N
@@ -83,27 +84,26 @@ class OrderStatistics:
         alphabet = list(MSA.amino_acid_dict(self.pickle).values())
         column_p = np.zeros(len(alphabet))
 
-        self.msa_profile = np.zeros((column_p.shape[0], msa.shape[1]))
+        frequencies = np.zeros((column_p.shape[0], msa.shape[1]))
 
         for j in range(msa.shape[1]):
             column_j = msa[:, j]
             for a in alphabet:
-                column_p[a] = np.where(column_j == a)[0].shape[0] / msa.shape[0]
+                frequencies[a, j] = np.where(column_j == a)[0].shape[0]
+            column_p = frequencies[:, j] / msa.shape[0]
             H[j] = -np.sum(column_p * safe_log(column_p))
-            # Store MSA profile for the future
-            self.msa_profile[:, j] = column_p
-        return H
+        return H, frequencies
 
-    def mutual_information(self, msa: np.ndarray, shannon: np.ndarray) -> np.ndarray:
+    def mutual_information(self, msa: np.ndarray, shannon: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         """
-        2nd order statistics
+        2nd order statistics/frequencies
         Computes mutual information of MSA given as:
             I_j,k = H_j + H_k - H_j,k
         where H_j,k is joint Shannon entropy
             H_j,k = - SUM_a( SUM_b( p^a,b_j,k * log(p^a,b_j,k)))
         """
         assert msa.shape[1] == shannon.shape[0]
-        n = msa.shape[0]
+        n = shannon.shape[0]
 
         # Shape according number of unique pairs in set
         I = np.zeros(n * (n - 1) // 2)
@@ -116,15 +116,20 @@ class OrderStatistics:
         for a in alphabet:
             pairs = [(a, b) for b in alphabet]
 
+        # Matrix, row number of MSA columns except last column which is included in previous frequencies count
+        # Row for pairs of amino acids
+        frequencies = np.zeros((shannon.shape[0]-1, column_jk_p.shape[0]))
+
         for j in range(msa.shape[1] - 1):
             for k in range(j + 1, msa.shape[1]):
                 # calculate H_j,k
                 for a, b in pairs:
                     vec_a, vec_b = np.where(msa[:, j] == a)[0], np.where(msa[:, k] == b)[0]
-                    column_jk_p[a * len(alphabet) + b] = sum([a in vec_b for a in vec_a]) / msa.shape[0]
+                    frequencies[j, a * len(alphabet) + b] = sum([a in vec_b for a in vec_a])
+                column_jk_p[:] = frequencies[j, :] / msa.shape[0]
                 H_j_k = -np.sum(column_jk_p)
                 I[j * msa.shape[1] + k] = shannon[j] + shannon[k] - H_j_k
-        return I
+        return I, frequencies
 
     def sample_dataset_from_normal(self, origin: np.ndarray, scale: float, N: int) -> np.ndarray:
         """ Sample from the model N data points and transform them to ndarray """
@@ -158,20 +163,25 @@ def run_setup():
     sampled_dataset = stat_obj.sample_dataset_from_normal(np.zeros(stat_obj.dimensions), 2.5, msa_dataset.shape[0])
 
     # 1st order stats
-    msa_shannon = stat_obj.shannon_entropy(msa_dataset)
-    sampled_shannon = stat_obj.shannon_entropy(sampled_dataset)
+    msa_shannon, msa_frequencies = stat_obj.shannon_entropy(msa_dataset)
+    sampled_shannon, sampled_frequencies = stat_obj.shannon_entropy(sampled_dataset)
 
     # 2nd order statistics
-    mutual_msa = stat_obj.mutual_information(msa_dataset, msa_shannon)
-    mutual_sampled = stat_obj.mutual_information(sampled_dataset, sampled_shannon)
+    mutual_msa, mutual_msa_frequencies = stat_obj.mutual_information(msa_dataset, msa_shannon)
+    mutual_sampled, mutual_sampled_frequencies = stat_obj.mutual_information(sampled_dataset, sampled_shannon)
 
-    # plot 1st order data statistics
+    # plot 1st order data statistics and frequencies statistics
     print("=" * 80)
     print(" Creating first and second order statistics to  {}".format(stat_obj.target_dir))
     stat_obj.plot_order_statistics(msa_shannon, sampled_shannon, 'Training Data Entropy', 'VAE Sampled Entropy',
                                    'first_order.png')
     stat_obj.plot_order_statistics(mutual_msa, mutual_sampled, 'Training Mutual Information',
                                    'Generated Mutual Information', 'second_order.png')
+    stat_obj.plot_order_statistics(msa_frequencies.flatten(), sampled_frequencies.flatten(),
+                                   'Training Data Frequencies', 'VAE Sampled Frequencie', 'first_order_frequencies.png')
+    stat_obj.plot_order_statistics(mutual_msa_frequencies.flatten(), mutual_sampled_frequencies.flatten(),
+                                   'Training Mutual Frequencies', 'Generated Mutual Frequencies',
+                                   'second_order_frequencies.png')
 
 
 if __name__ == "__main__":
