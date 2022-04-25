020849973e222bf4b60421fcffb101cc16d5ed03
Pavel Kohout <xkohou15@stud.fit.vutbr.cz>
2022-04-25 11:25:53 +0200

    efactoring piliner
    Signed-off-by: Pavel Kohout <xkohou15@stud.fit.vutbr.cz>

--- vae_our_approach/devel_LL/pipeline_importer.py
+++ vae_our_approach/devel_LL/pipeline_importer.py
@@ -5,12 +5,12 @@ for var in ['PIP_CAT', 'PIP_PAPER', 'PIP_SCORE']:
         print("The os environment variables were not set!! Variable : ", var)
         exit(1)
 if os.environ['PIP_CAT'] == 'True':
-    from catalytic_res_msa_filter import CatalyticMSAPreprocessor as MSA
+    from msa_handlers.catalytic_res_msa_filter import CatalyticMSAPreprocessor as MSA
 elif os.environ['PIP_PAPER'] == 'True' or os.environ['PIP_SCORE'] == 'False':
     # Default variant in the case of setting just --no_score_filter param
-    from msa_preparation import MSA as MSA
+    from msa_handlers.msa_preparation import MSA as MSA
 elif os.environ['PIP_SCORE'] == 'True':
-    from msa_preprocessor import MSAPreprocessor as MSA
+    from msa_handlers.msa_preprocessor import MSAPreprocessor as MSA
 
 ## Unset linux environment variable
 del os.environ['PIP_SCORE']
