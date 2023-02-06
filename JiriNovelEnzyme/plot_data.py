8150e640f04e6f683f9ebcccc27a129f268dc1be
Pavel Kohout <xkohou15@stud.fit.vutbr.cz>
2023-02-06 10:16:29 +0100

    Pipeliner for MSA projection preprocessing

--- JiriNovelEnzyme/plot_data.py
+++ JiriNovelEnzyme/plot_data.py
@@ -13,7 +13,13 @@ from adjustText import adjust_text
 
 def get_indexes_of_accessions(accessions_list, accessions):
     """ Get just indexes to embeddings mus values """
-    indexes = [accessions_list.index(acc) for acc in accessions]
+    # indexes = [accessions_list.index(acc) for acc in accessions]
+    indexes = []
+    for acc in accessions:
+        try:
+            indexes.append(accessions_list.index(acc))
+        except:
+            pass
     return indexes
 
 
@@ -48,8 +54,25 @@ black_acc_to_name = {row['Accession No.']: row['Name'] for _, row in df_black.it
 black_name_to_acc = {row['Name']: row['Accession No.'] for _, row in df_black.iterrows()}
 assert len(black_vals) == len(list(black_name_to_acc.values()))
 
+# Read Hon subfamilies
+dfhon = pd.read_excel("Hon_2021_HTS2_selection_dataset_from_2017.xlsx", sheet_name="FULL DATASET")
+accessions1 = dfhon.loc[dfhon["Subfamily"] == 1]["Accession"].tolist()
+accessions2 = dfhon.loc[dfhon["Subfamily"] == 2]["Accession"].tolist()
+accessions3 = dfhon.loc[dfhon["Subfamily"] == 3]["Accession"].tolist()
+accessions4 = dfhon.loc[dfhon["Subfamily"] == 4]["Accession"].tolist()
+
 with open("embeddings.pkl", "rb") as file:
     embeddings = pickle.load(file)
+
+# Subfamilies coordinates
+# coordinates_sub1 = get_indexes_of_accessions(accessions_list=embeddings['keys'], accessions=accessions1)
+# coordinates_sub2 = get_indexes_of_accessions(accessions_list=embeddings['keys'], accessions=accessions2)
+# coordinates_sub3 = get_indexes_of_accessions(accessions_list=embeddings['keys'], accessions=accessions3)
+# coordinates_sub3 = get_indexes_of_accessions(accessions_list=embeddings['keys'], accessions=accessions3)
+
+coords_list = []
+for acc_sub in [accessions1, accessions2, accessions3, accessions4]:
+    coords_list.append(get_indexes_of_accessions(accessions_list=embeddings['keys'], accessions=acc_sub))
 
 green_idx = get_indexes_of_accessions(embeddings['keys'], list(green_acc_to_name.keys()))
 red_idx = get_indexes_of_accessions(embeddings['keys'], list(red_acc_to_name.keys()))
@@ -70,4 +93,15 @@ for idx, c, labels in [(green_idx, 'g', green_vals), (red_idx, 'r', red_vals),
 
 adjust_text(ts, x=embeddings['mu'][:, 0], y=embeddings['mu'][:, 1],
             force_points=0.2, arrowprops=dict(arrowstyle='-', color='black'))
+plt.show()
+
+# Plot subfamilies
+# plt.scatter(embeddings['mu'][coords_list[0], 0], embeddings['mu'][coords_list[0], 1], marker='o', edgecolors='black', color='#F1F2E1')
+# plt.scatter(embeddings['mu'][coords_list[1], 0], embeddings['mu'][coords_list[1], 1], marker='o', edgecolors='black', color='#EDEBFE')
+# plt.scatter(embeddings['mu'][coords_list[2], 0], embeddings['mu'][coords_list[2], 1], marker='o', edgecolors='black', color='#EEFBFA')
+# plt.scatter(embeddings['mu'][coords_list[3], 0], embeddings['mu'][coords_list[3], 1], marker='o', edgecolors='black', color='#FFF4F7')
+plt.scatter(embeddings['mu'][coords_list[0], 0], embeddings['mu'][coords_list[0], 1], marker='o', edgecolors='black', color='red')
+plt.scatter(embeddings['mu'][coords_list[1], 0], embeddings['mu'][coords_list[1], 1], marker='o', edgecolors='black', color='blue')
+plt.scatter(embeddings['mu'][coords_list[2], 0], embeddings['mu'][coords_list[2], 1], marker='o', edgecolors='black', color='green')
+plt.scatter(embeddings['mu'][coords_list[3], 0], embeddings['mu'][coords_list[3], 1], marker='o', edgecolors='black', color='yellow')
 plt.show()
---
+++ vae_our_approach/scripts/msa_handlers/projection_msa.py
@@ -0,0 +1,25 @@
+__author__ = "Pavel Kohout xkohou15@vutbr.cz"
+__date__ = "2023/02/06"
+__description__ = "MSA preprocessing step for the projection of of dataset into the latent space while perceiving as " \
+                  "many sequences as possible. " \
+                  "Does not apply classical steps of the MSA preprocessing that reduce the number of sequences " \
+                  "significantly. Only sequences with low MSA sequential overlap with query are excluded (<50%)"
+
+import inspect
+import os
+import sys
+
+currentDir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentDir = os.path.dirname(currentDir)
+sys.path.insert(0, parentDir)
+
+from parser_handler import CmdHandler
+from msa_handlers.msa_preprocessor import MSAPreprocessor
+
+# Get command line arguments and prepare directory
+cmd_liner = CmdHandler()
+msa = MSAPreprocessor(setuper=cmd_liner)
+msa.simple_preprocess_msa()
+print("#" * 80)
+print(f"     MSA for projection is ready in {cmd_liner.exp_dir}")
+print("#" * 80)
